ca740581622a6f57fbed3d6c2b69a85adc22e44f
VICENTVANGOG <165677383+VICENTVANGOG@users.noreply.github.com>
2024-09-21 17:52:58 -0500

    jose miguel : login for facebook

--- src/app/componets/Login-componets/buttonfacebook.tsx
+++ src/app/componets/Login-componets/buttonfacebook.tsx
@@ -5,6 +5,20 @@ import { getAuth, signInWithPopup, FacebookAuthProvider } from 'firebase/auth';
 import { FaFacebook } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyB4c7FpScGrs4F2n1Z9uzpAaGBwyVPkaJA",
+  authDomain: "pawhome-ba1cd.firebaseapp.com",
+  projectId: "pawhome-ba1cd",
+  storageBucket: "pawhome-ba1cd.appspot.com",
+  messagingSenderId: "580367349831",
+  appId: "1:580367349831:web:82fa2da311ffa13089d962",
+  measurementId: "G-EBX3ENQ2WM"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const provider = new FacebookAuthProvider();
 
 const Button = styled.button`
@@ -31,19 +45,6 @@ const Button = styled.button`
 `;
 
 const LoginButton: React.FC = () => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyB4c7FpScGrs4F2n1Z9uzpAaGBwyVPkaJA",
-    authDomain: "pawhome-ba1cd.firebaseapp.com",
-    projectId: "pawhome-ba1cd",
-    storageBucket: "pawhome-ba1cd.appspot.com",
-    messagingSenderId: "580367349831",
-    appId: "1:580367349831:web:82fa2da311ffa13089d962",
-    measurementId: "G-EBX3ENQ2WM"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-
   const callLoginFacebook = async () => {
     // Muestra el SweetAlert de Cargando
     Swal.fire({
@@ -57,31 +58,28 @@ const LoginButton: React.FC = () => {
     try {
       const result = await signInWithPopup(auth, provider);
       const credential = FacebookAuthProvider.credentialFromResult(result);
-      if (credential) {
-        const token = credential.accessToken;
-        const user = result.user;
+      const token = credential?.accessToken ?? ""; // Asigna un valor vacío si es undefined
+      const user = result.user;
+      const displayName = user.displayName ?? "Usuario"; // Asigna "Usuario" si es undefined
 
-        const displayName = user.displayName;
+      // Guardar en localStorage
+      localStorage.setItem('token', token);
 
-        console.log('Token:', token);
-        console.log('Nombre:', displayName);
-
-        Swal.close();
-        Swal.fire({
-          title: `Bienvenido a PawHome, ${displayName}!`,
-          imageUrl: '/icons/logo.png', // Coloca aquí la URL de tu imagen
-          imageWidth: 100,
-          imageHeight: 100,
-          icon: 'success',
-          willClose: () => {
-            window.location.href = '/'; // Redirige a la página de inicio
-          },
-          background: 'linear-gradient(to right, #F25FFFFF, #CE7BFEFF)', // Degradado de fondo
-          customClass: {
-            popup: 'custom-popup'
-          }
-        });
-      }
+      Swal.close();
+      Swal.fire({
+        title: `Bienvenido a PawHome, ${displayName}!`,
+        imageUrl: '/icons/logo.png', // Coloca aquí la URL de tu imagen
+        imageWidth: 100,
+        imageHeight: 100,
+        icon: 'success',
+        willClose: () => {
+          window.location.href = '/'; // Redirige a la página de inicio
+        },
+        background: 'linear-gradient(to right, #F25FFFFF, #CE7BFEFF)', // Degradado de fondo
+        customClass: {
+          popup: 'custom-popup'
+        }
+      });
     } catch (error) {
       console.error('Error:', error);
       Swal.close(); // Cierra el loading alert en caso de error
